94ab69d1aa38d0edca13faf50324cc1c8fab34b7
heavenlyflightless-pixel <heavenlyflightless@gmail.com>
2026-07-22 18:57:34 -0700

    plamovedata, status and stats

--- data/mods/gen8legends/scripts.ts
+++ data/mods/gen8legends/scripts.ts
@@ -1,4 +1,12 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 8,
 	inherit: 'gen8',
+	init() {
+		// Legends: Arceus has neither held items nor ordinary Abilities. Individual
+		// exceptions (Cherrim and Regigigas) are implemented as format mechanics.
+		this.modData('Abilities', 'noability').isNonstandard = null;
+		for (const id in this.data.Pokedex) {
+			this.modData('Pokedex', id).abilities = { 0: 'No Ability' };
+		}
+	},
 };
